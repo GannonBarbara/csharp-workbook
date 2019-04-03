e2f7f369ffa8ee3beffbed1b3b0c3fc5ee5f3e2b
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-04-02 20:25:43 -0500

    my csharp greetings program week 1

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            Console.WriteLine("Hello");
+            Console.WriteLine("My name is Gannon");
+            Console.WriteLine("What is your name? ");
+            String yourName = Console.ReadLine();
+            // Console.WriteLine("Nice to meet you "+yourName);
+            if(yourName == "Bob"){
+                Console.WriteLine("Really, that's the best you could come up with?");
+            } else {
+                Console.WriteLine("Hello "+yourName);
+            }
+
+
+
         }
     }
 }
