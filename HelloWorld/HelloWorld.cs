7e2ece4ee1ef6b3f79b23f2f90d4402c1510aeac
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-04-04 18:38:30 -0500

    pushed up homework project

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,19 +6,35 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-          
-            Console.WriteLine("Hello");
-            Console.WriteLine("My name is Gannon");
-            Console.WriteLine("What is your name? ");
-            String yourName = Console.ReadLine();
-            // Console.WriteLine("Nice to meet you "+yourName);
-            if(yourName == "Bob"){
-                Console.WriteLine("Really, that's the best you could come up with?");
-            } else {
-                Console.WriteLine("Hello "+yourName);
-            }
+        //    //defining string
+        //     string userInput;
+        //     //defining variable val1
+        //     int val1;
+        //     //defining variable val2
+        //     int val2;
+
+        //     Console.Write("Enter integer value: ");
+        //     userInput = Console.ReadLine();
+        //     val1 = Convert.ToInt32(userInput);
+        //     Console.WriteLine(val1);
+
+        //     Console.Write("Enter second integer value: ");
+        //     userInput = Console.ReadLine();
+        //     val2 = Convert.ToInt32(userInput);
+        //     Console.WriteLine(val1, "+"+val2);
+
+        string userInput;
+        char UserName;
+
+        Console.Write("Hello, what is your name?");
+        userInput = Console.ReadLine();
+        Console.WriteLine("Nice to meet you, "+userInput);
+        
 
 
+
+
+     
 
         }
     }
