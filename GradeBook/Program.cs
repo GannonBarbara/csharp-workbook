e421464d9bae810e459c26543f3bace6623a6be3
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-04-18 21:06:18 -0500

    worked on loops & gradebook assignment

---
+++ GradeBook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // // defined a new dictionary that has strings as the key
+            // // and strings as the value
+
+            // Dictionary<String, String> students = new Dictionary<String, String>();
+
+            // // defines a new dictionary that has strings as the key
+            // // and the value is an array of strings
+
+            // Dictionary<String, String[]> gradebook = new Dictionary<String, String[]>();
+
+            // int x = 2;
+            // String color = "Red";
+
+            // List<String> fruits = new List<String>();
+            // fruits.Add("Apple");
+            // fruits.Add("Banana");
+
+            // Stack<String> lifo = new Stack<String>();
+            // lifo.Push("A");
+            // lifo.Push("B");
+
+
+            for (int i = 0; i<5; i++)
+            {           
+            Console.WriteLine("Student name?");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("4 student grades? Separate with space.");
+            string studentGrades = Console.ReadLine();
+            string[] grades = studentGrades.Split(" ");
+            Console.WriteLine(String.Join(" ", grades));
+            }
+        }
+    }
+}
---
+++ LoopQuiz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LoopQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] nums = new int {3,6,7,1567,8,23,45,57,145,89,93,97,106,111,202};
+            int maxSoFar = nums[0];
+            for (int idx = 0; idx < nums.Length; idx++)
+            {
+                int currentNum = nums[idx];
+                Console.WriteLine(idx+"=>"+currentNum);
+                if (maxSoFar < currentNum)
+                {
+                    maxSoFar = currentNum;
+                }
+            }
+            for (int i=0;i<nums.Length;i++)
+            {
+                if ( )
+                {
+
+                }
+            }
+        }
+    }
+}
