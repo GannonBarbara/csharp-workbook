092dcfffddcd8f3c960e1a8bb08d9e6ef59f4353
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-04-20 16:01:15 -0500

    completed fizzbuzz

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,37 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 1000; i++)
+            // i loops through incriments of 1 integer
+            // i loops up to 1000
+            // i is increasing
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                // if the remainder if i divided by 3 is zero
+                // and the remainder of i divided by 5 is zero
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i % 3 == 0)
+                // if i is not divisible by 3 and five with the remainder of zero
+                // but it is divisible by 3 with the remainder of zero
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 ==0)
+                // if i is not divisible by 3 and 5 with the remainder of zero
+                // but it is divisible by 5 with the remainder of zero
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                // if none of the conditions above apply
+                {
+                    Console.WriteLine(i);
+                    // display the integer value of i
+                }
+            }
         }
     }
 }
