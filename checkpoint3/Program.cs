99f1b0dce6527c618f544046153bae294602d9a7
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-06-04 15:53:38 -0500

    todo app re-do

---
+++ checkpoint3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace checkpoint3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Controller // user interaction 
+    {
+
+    }
+    public class DataAccess // what to do with items on list
+    {
+        public List<Items> ToDos {public get; private set;}
+
+    }
+    public class Items // properties of items on list
+    {
+        public int id = 0;      
+
+
+    }
+}
