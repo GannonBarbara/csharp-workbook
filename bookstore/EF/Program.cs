eef9388804991569c040c70adb94397ed278a52f
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-06-04 16:14:37 -0500

    bookstore practice

---
+++ bookstore/EF/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dao dao = new Dao();
+            dao.create("Cat in the Hat", Type.Kids);
+            dao.create("The Hobbit", Type.Fiction);
+
+            Console.WriteLine("Book title:");
+            String UserInputTitle = Console.ReadLine();
+            Console.WriteLine("Book type:");
+            String UserInputType = Console.ReadLine();
+
+            dao.create(UserInputTitle, UserInputType);
+
+            
+            foreach(Book b in dao.listBooks())
+            {
+                Console.WriteLine(b.title+" is a "+b.type+" book");
+            }
+        }
+    }
+    
+}
