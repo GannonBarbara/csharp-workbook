944f2eb11c06535751e8594f6a39800606404c4b
Gannon Berriman <gannon.berriman@g.austincc.edu>
2019-06-10 18:03:20 -0500

    not sure if db is set up correctly

--- checkpoint3/Program.cs
+++ checkpoint3/Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using System.Linq;
 
 namespace checkpoint3
 {
-    class Program
+    public class App
     {
         static void Main(string[] args)
         {
@@ -14,32 +15,26 @@ namespace checkpoint3
             app.start();
             
         }
-    }
-    public class Controller // not sure necessary--different from app?
-    {
-
-    }
-    public class App //facilitates program/gets things started/ calls UI class
-    {
-        Dao dao;
-        UI ui;
-        public App()
-        {
-            dao = new Dao();
-            ui = new UI();
-        }
+        public Dao dao;
+        public UI ui;
         public void start()
         {
             ui.PrintMenu();
+            ui.GetInput();
 
         }
+        public App()
+        {
+            Dao dao = new Dao();
+            ui = new UI();
+        }
     }
-    public class Dao // what to do with items on list
+    public class Dao //facilitates program/gets things started/ calls UI class. Tells what to do w/ logic in dao & UI
     {
-        public List<Items> ToDos { get; private set;}
-        public ToDoContext Context {get; set;}
+        public List<Items> ToDos;
+        public UI ui;
 
-        public UI ui{get; set;}
+        public ToDoContext Context {get; set;}
         public Dao()
         {
             Context = new ToDoContext();
@@ -59,12 +54,9 @@ namespace checkpoint3
         {
 
         }
-        public void CreateItem() // enum here?
+        public void CreateItem(String task) // logic in app class
         { 
-            Console.WriteLine("Enter new to-do item:");
-            string UInput = Console.ReadLine();
-            ui.GetInput(UInput);
-            Items item = new Items(ui.input);
+            Items item = new Items(task);
             Context.ToDoItems.Add(item);
             Context.SaveChanges();
         }
@@ -76,14 +68,12 @@ namespace checkpoint3
             }
         }
 
-        public void SelectItem()
+        public IQueryable<Items> SelectItem(int id)
         {
-            if (ui.input == "Select" || ui.input =="select")
-            {
-                Console.WriteLine("Which #?");
-                string selection = Console.ReadLine();
-
-            }
+            var item = from i in Context.ToDoItems
+                where i.id.Equals(id)
+                select i;
+            return item;
         }
         public void DeleteItem()
         {
@@ -93,8 +83,7 @@ namespace checkpoint3
     }
     public class UI // user interaction
     {
-        public String input {get; set;}
-        public Dao dao;
+        Dao dao = new Dao();
         public void PrintMenu()
         {
             Console.WriteLine("List of Commands:");
@@ -108,17 +97,31 @@ namespace checkpoint3
             string input = Console.ReadLine();
             if (input == "New" || input == "new")
             {
-                dao.CreateItem();
+                Console.WriteLine("Enter task:");
+                String NewTask = Console.ReadLine();
+                dao.CreateItem(NewTask);
+            }
+            if (input == "Select" || input == "select")
+            {
+                Console.WriteLine("Which #?");
+                int id = Convert.ToInt32(Console.ReadLine());
+                dao.SelectItem(id);
+            }
+            if (input == "Show" || input == "show")
+            {
+                Console.WriteLine("To-do List:");
+                foreach (Items item in dao.Context.ToDoItems)
+                {
+                   Console.WriteLine("{0} {1} {2}", item.id, item.ToDo, item.status);
+                }
+                Console.WriteLine();
+
             }
         }
-        public string GetInput(string input) // how connect to printmenu()
+        public string GetInput() // how connect to printmenu()
         {
-            input = Console.ReadLine();
+            String input = Console.ReadLine();
             return input;
-        }
-        public void printMessage()
-        {
-
         }
         public void PrintError()
         {
@@ -126,22 +129,20 @@ namespace checkpoint3
         }
     }
     public class Items // properties of items on list
-    {
-        
+    {       
         public int id  {get; set;}
         public String ToDo {get; set;} // contains content of todo item
+        public Status status {get; set;}
         public Items()
-        {
-
-        }
+        {}
         public Items(String iToDo)
         {
             this.ToDo = iToDo;
-        }
-        public enum Status
-        {
-           done, pending 
-        }
+        }  
+    }
+    public enum Status
+    {
+        done, pending
     }
     public class  ToDoContext : DbContext
     {
